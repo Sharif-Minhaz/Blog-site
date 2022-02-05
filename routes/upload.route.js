f63491695761e6a217c21298be68a6fbb8d92b61
Minhaz <sharif35-3001@diu.edu.bd>
2022-02-05 22:57:23 +0600

    image upload for tiny mce done

--- routes/upload.route.js
+++ routes/upload.route.js
@@ -3,10 +3,16 @@ const router = require("express").Router();
 const { isAuthenticated } = require("../middlewares/auth.middleware");
 const upload = require("../middlewares/upload.middleware");
 
-const { uploadProfilePics, removeProfilePics } = require("../controllers/upload.controller");
+const {
+	uploadProfilePics,
+	removeProfilePics,
+	postImageUploadController,
+} = require("../controllers/upload.controller");
 
 router.post("/profilePics", isAuthenticated, upload.single("profilePics"), uploadProfilePics);
 
 router.delete("/profilePics", isAuthenticated, removeProfilePics);
+
+router.post("/postimage", isAuthenticated, upload.single("post-image"), postImageUploadController);
 
 module.exports = router;
