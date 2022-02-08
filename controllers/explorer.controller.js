d627f667093b0c68f7f9c9f49738eb83a2b729f9
Minhaz <sharif35-3001@diu.edu.bd>
2022-02-08 19:23:20 +0600

    explorer complete

--- controllers/explorer.controller.js
+++ controllers/explorer.controller.js
@@ -1,10 +1,17 @@
 const Flash = require("../utils/Flash");
+const Post = require("../models/Post.model");
 
-exports.explorerGetController = (req, res, next) => {
-	res.render("pages/explorer/explorer", {
-		title: "Explore Posts",
-		filter: "latest",
-		error: {},
-		flashMessage: Flash.getMessage(req),
-	});
+exports.explorerGetController = async (req, res, next) => {
+	try {
+		let posts = await Post.find();
+		res.render("pages/explorer/explorer", {
+			title: "Explore Posts",
+			filter: "latest",
+			error: {},
+			flashMessage: Flash.getMessage(req),
+            posts
+		});
+	} catch (err) {
+		next(err);
+	}
 };
