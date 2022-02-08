7168d37989f6faa818b1fbc4c90b5367720d18e9
Minhaz <sharif35-3001@diu.edu.bd>
2022-02-08 20:28:08 +0600

    filter feature added

--- controllers/explorer.controller.js
+++ controllers/explorer.controller.js
@@ -1,15 +1,62 @@
 const Flash = require("../utils/Flash");
 const Post = require("../models/Post.model");
+const moment = require("moment");
+
+function genDate(days) {
+	let date = moment().subtract(days, "days");
+	return date.toDate();
+}
+
+function generateFilterObject(filter) {
+	let filterObj = {};
+	let order = 1;
+
+	switch (filter) {
+		case "week": {
+			filterObj = {
+				createdAt: {
+					$gt: genDate(7),
+				},
+			};
+			order = -1;
+			break;
+		}
+		case "month": {
+			filterObj = {
+				createdAt: {
+					$gt: genDate(30),
+				},
+			};
+			order = -1;
+			break;
+		}
+		case "all": {
+			order = -1;
+			break;
+		}
+	}
+
+	return {
+		filterObj,
+		order,
+	};
+}
 
 exports.explorerGetController = async (req, res, next) => {
+	let filter = req.query.filter || "latest";
+
+	let { order, filterObj } = generateFilterObject(filter.toLowerCase());
+
 	try {
-		let posts = await Post.find();
+		let posts = await Post.find()
+			.populate("author", "username")
+			.sort(order === 1 ? "-createdAt" : "createdAt");
 		res.render("pages/explorer/explorer", {
 			title: "Explore Posts",
-			filter: "latest",
+			filter,
 			error: {},
 			flashMessage: Flash.getMessage(req),
-            posts
+			posts,
 		});
 	} catch (err) {
 		next(err);
